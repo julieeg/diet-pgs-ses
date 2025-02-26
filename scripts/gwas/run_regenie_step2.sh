25a179d83fb2dbbe447515b7f144158fbb942188
Julie E. Gervis <64417667+julieeg@users.noreply.github.com>
2025-02-26 08:40:52 -0500

    Create run_regenie_step2.sh

---
+++ scripts/gwas/run_regenie_step2.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#$ -l h_vmem=50G
+#$ -l h_rt=6:00:00
+#$ -o reports/
+
+#$ -j y
+#$ -cwd
+
+
+CHR=$SGE_TASK_ID
+
+
+pheno_file=../data/processed/gwas/ukb_phenos_gwas_macros_MA.txt
+covar_file=../data/processed/gwas/ukb_phenos_gwas_covariates_MA.txt
+sample_file=../data/processed/gwas/ukb_phenos_gwas_macros_MA_samples.txt
+
+
+ukb_sample=/humgen/florezlab/UKBB_app27892/ukb27892_imp_chrAUT_v3_s487395.sample
+ukb_bgen_dir=/broad/ukbb/imputed_v3 
+
+
+scratch=/broad/hptmp/gervis
+gwas_dir=../data/processed/gwas
+opt=../../opt
+
+
+
+source /broad/software/scripts/useuse
+use .regenie-3.2.2
+
+
+
+##################
+## regenie GWAS ##
+##################
+
+## run REGENIE Step 1 
+regenie \
+  --step 2 \
+  --bgen ${scratch}/chr${CHR}_maf001_imp04.bgen \
+  --ref-first \
+  --sample ${ukb_sample} \
+  --keep ${sample_file} \
+  --phenoFile ${pheno_file} \
+  --covarFile ${covar_file} \
+  --catCovarList geno_array,smoke_current,smoke_former,alcohol,health \
+  --test additive \
+  --bsize 1000 --firth --approx --firth-se \
+  --no-split \
+  --lowmem --lowmem-prefix ../data/temp/ukb_chr${CHR}_macros_MA_step2 \
+  --pred ${gwas_dir}/ukb_gwas_macros_MA_step1_pred.list \
+  --out ../data/processed/gwas/ukb_chr${CHR}_macros_MA_step2 \
+  --threads 8
+
+
+#EOF
+
+
